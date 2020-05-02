db1dd9309b426eaf47498cc2ddf64bd4e702cfa3
flow0284 <eflo84@freenet.de>
2020-05-02 12:58:21 +0200

    Update get-translations.sh

--- .github/workflows/get-translations.sh
+++ .github/workflows/get-translations.sh
@@ -3,11 +3,11 @@ function get-translations(){
     curl -H "X-Api-Token: 48953334-3b8e-4f2c-92d5-801d83282a7d" -X GET -H \
     "Content-type: application/json" \
     >temp.lua \
-    "ttps://wow.curseforge.com/api/projects/31873/localization/export?lang=$1"
+    "https://wow.curseforge.com/api/projects/31873/localization/export?lang=$1"
 
     sed -e '/--.*$/{r temp.lua' -e 'd}' MillButton/locales/locale.$1.lua >temp2.lua
     awk '{gsub("\\\\\\\\n", "\\n", $0); print}' temp2.lua >temp.lua
-    mv temp.lua MillButton/Locales/$1.lua
+    mv temp.lua MillButton/Locales/locale.$1.lua
     rm temp2.lua
 }
 get-translations $1
